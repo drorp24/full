e4a763725271050789e9b6e814f33507e1dfdc57
Dror <drorp24@gmail.com>
2019-07-09 18:39:04 +0300

    Card expansion improved

--- src/components/app/Merchant.js
+++ src/components/app/Merchant.js
@@ -15,9 +15,10 @@
 //    (as demonstrated in https://uxdesign.cc/good-to-great-ui-animation-tips-7850805c12e5)
 //    This means that
 //    - AppBar needs to contract away, signalling a 'fullscreen' mode
-//    - card's sibling needs to be pushed away down rather than covered by the card
+//    - both card's siblings need to be pushed away to make toom for the expanding card
 //    'toggleCardState' achieves the first by updating an application-wide (redux) state of 'fullscreen'
-//    and the second by literally pushing the next sibling down, only to return it back to its original place when card gets closed.
+//    and the second by manipulating their 'height's and 'top's back and forth.
+//    Note: react-window's own .scroll api will not animate. Instead, I'm modifying the list-item's top properties and height if needed.
 
 import React, { useState, useCallback } from 'react'
 import { useDispatch } from 'react-redux'
@@ -32,10 +33,14 @@ import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
+import Fab from '@material-ui/core/Fab'
+import ListAlt from '@material-ui/icons/ListAlt'
+import Zoom from '@material-ui/core/Zoom'
 
 import Loader from '../utility/Loader'
 import { ellipsis } from '../themed/Box'
 
+const appBarHeight = window.innerHeight / 10
 // makeStyles accepts a 'theme' argument and returns another function that optionally accepts the component's props (or anything really)
 // this is by far the best way to define styling rules in a dynamic way, i.e., as a function of some changing props (Requires MUI v4)
 const useStyles = makeStyles(theme => ({
@@ -43,6 +48,9 @@ const useStyles = makeStyles(theme => ({
     height: '100%',
     width: '100%',
     transition: 'height 1s',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
   },
   media: {
     height: ({ open }) => (open ? '50vh' : '20vh'),
@@ -51,22 +59,24 @@ const useStyles = makeStyles(theme => ({
   listItem: {
     height: ({ open }) => (open ? '100vh' : '100%'),
     padding: ({ open }) => (open ? 0 : theme.spacing(2)),
-    zIndex: ({ open }) => {
-      if (open) return 1
-      else {
-        // even when card contracts back, it still needs to cover its siblings
-        // this awaits the animation to complete before reverting the z-index back to 0
-        // a better way of doing it would be to set a 3rd value to state: animation complete - and use that value here (as in 'drag & drop')
-        setTimeout(function() {
-          return 0
-        }, 1000)
-      }
-    },
+    zIndex: ({ open }) => (open ? 1 : 0),
     transition: 'padding 1s, height 1s, top 1s',
     justifyContent: 'center',
   },
   price: {
     fontWeight: '400',
+  },
+  viewOfferButton: {
+    display: ({ open }) => (open ? 'none' : 'block'),
+  },
+
+  fab: {
+    visibility: ({ open }) => (open ? 'visible' : 'hidden'),
+    position: 'absolute',
+    margin: theme.spacing(2),
+    marginTop: 0,
+    top: 'calc(50vh - 28px)',
+    right: 0,
   },
 }))
 
@@ -75,7 +85,7 @@ const useStyles = makeStyles(theme => ({
 const Merchant = ({ loading, record, style, listRef, index }) => {
   //
   const [state, setState] = useState({ open: false })
-  window.setState = setState
+  const { open } = state
 
   const dispatch = useDispatch()
   const updateApp = useCallback(app => dispatch(setApp(app)), [dispatch])
@@ -92,30 +102,86 @@ const Merchant = ({ loading, record, style, listRef, index }) => {
 
     // toggleCardState implements MD recommended card expansion: https://uxdesign.cc/good-to-great-ui-animation-tips-7850805c12e5
     const toggleCardState = () => {
-      // Pushing sibling away (and returning it) has to be done imperatively as it entails modifying the DOM elements
-      const pushSiblingAway = sibling => {
-        const top = Number(sibling.style.top.replace('px', ''))
-        const height = Number(sibling.style.height.replace('px', ''))
-        const newTop = top + height
-        sibling.setAttribute('data-top', `${top}px`)
-        //! Never do x.setAttribute('style', '...') since that would override other style attributes. Do the following:
-        sibling.style.top = `${newTop}px`
+      // Pushing siblings away (and returning them) has to be done imperatively as it entails modifying their respective DOM elements
+      const pushSiblingsAway = (
+        previousSibling,
+        currentSibling,
+        nextSibling
+      ) => {
+        //
+        const measureTopHeight = element => {
+          if (!element) return { element: null }
+          const top = Number(element.style.top.replace('px', ''))
+          const height = Number(element.style.height.replace('px', ''))
+          const y = element.getBoundingClientRect().y
+          return { element, top, height, y }
+        }
+
+        const setTopHeight = ({ element, top, newTop, height, newHeight }) => {
+          if (!element) return
+
+          const defined = property =>
+            typeof property !== 'undefined' && property !== null // we don't want to update DOM unnecessarily, but '0' is a value
+
+          if (defined(newTop)) {
+            element.setAttribute('data-top', `${top}px`)
+            element.style.top = `${newTop}px` // Never x.setAttribute('style', '...') as it would override other style properties
+          }
+          if (defined(newHeight)) {
+            element.setAttribute('data-height', `${height}px`)
+            element.style.height = `${newHeight}px`
+          }
+        }
+
+        //! Transition
+        // I'm assuming maximum 3 items in a viewport, otherwise this should be done in a loop
+        const [previous, current, next] = [
+          previousSibling,
+          currentSibling,
+          nextSibling,
+        ].map(measureTopHeight)
+
+        previous.newTop = Math.max(previous.top - current.y, 0)
+        if (previous.newTop === 0)
+          previous.newHeight = Math.max(previous.height - current.y, 0) // if there's no room to retreat, contract
+        current.newTop = Math.max(current.top - current.y + appBarHeight, 0)
+        next.newTop = next.top + (window.innerHeight - next.y + appBarHeight)
+
+        for (let item of [previous, current, next]) {
+          setTopHeight(item)
+        }
       }
 
-      const returnSiblingToPlace = sibling => {
-        const originalTop = sibling.getAttribute('data-top')
-        if (originalTop) sibling.style.top = originalTop
+      const restoreTopHeight = element => {
+        if (!element) return
+        const originalTop = element.getAttribute('data-top')
+        if (originalTop) element.style.top = originalTop
+        const originalHeight = element.getAttribute('data-height')
+        if (originalHeight) element.style.height = originalHeight
       }
 
-      const { open } = state
+      const returnSiblingsToPlace = (
+        previousSibling,
+        currentSibling,
+        nextSibling
+      ) => {
+        for (let element of [previousSibling, currentSibling, nextSibling]) {
+          restoreTopHeight(element)
+        }
+      }
+
+      //  Tried setTimeout'ing a 'transition' state to have the FAB pop up only once transition finishes
+      //  It sat in the same state object with 'state' since MUI's makeStyle accepts only one prop argumement.
+      //  I ended up abandoning it since it made the component re-render and the animation to flicker.
       updateApp({ fullscreen: !open })
       setState({ open: !open })
-      listRef.current.scrollToItem(index + 1, 'end')
 
-      // state will not have changed yet when these lines are executed.
-      // so 'open' actually means "about to change to close"
-      const sibling = listItemRef.current.nextSibling
-      open ? returnSiblingToPlace(sibling) : pushSiblingAway(sibling)
+      const { previousSibling, nextSibling } = listItemRef.current
+      const currentSibling = listItemRef.current
+
+      !open
+        ? pushSiblingsAway(previousSibling, currentSibling, nextSibling)
+        : returnSiblingsToPlace(previousSibling, currentSibling, nextSibling)
     }
 
     return (
@@ -157,13 +223,23 @@ const Merchant = ({ loading, record, style, listRef, index }) => {
           </CardContent>
         </CardActionArea>
         <CardActions>
-          <Button size="small" color="primary">
+          <Button size="small" color="primary" className={classes.orderButton}>
             Order
           </Button>
-          <Button size="small" color="primary" onClick={toggleCardState}>
-            {state && state.open ? 'Close' : 'View Offer'}
+          <Button
+            size="small"
+            color="primary"
+            onClick={toggleCardState}
+            className={classes.viewOfferButton}
+          >
+            View
           </Button>
         </CardActions>
+        <Zoom in timeout={{ enter: 1000 }}>
+          <Fab color="primary" aria-label="Add" className={classes.fab}>
+            <ListAlt />
+          </Fab>
+        </Zoom>
       </Card>
     )
   }
