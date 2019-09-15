97248ef2d29bd53eadbc2d71a31573d55bc7e33f
Dror <drorp24@gmail.com>
2019-09-15 18:13:08 +0300

    https

--- src/components/app/Merchants.js
+++ src/components/app/Merchants.js
@@ -8,37 +8,36 @@ import merchantsQuery, {
 } from '../../queries/merchantsQuery'
 import QueryResponse from '../utility/QueryResponse'
 import Merchant from './Merchant'
-import NoSsr from '@material-ui/core/NoSsr'
 
+// Apollo client now enables getting the results ({data, loading etc}) by calling useQuery with the query as its arguments
+// Query isn't needed anymore  https://www.apollographql.com/docs/react/features/pagination/#cursor-based
 const Merchants = () => {
   // Look ma, no connect!
   const form = useSelector(state => state.form)
   const merchantsQueryVariables = mapFormToMerchantQueryVariables(form)
   return (
-    <NoSsr>
-      <Page title="Merchants">
-        <Query
-          query={merchantsQuery}
-          variables={merchantsQueryVariables}
-          fetchPolicy="cache-and-network"
-          errorPolicy="all"
-          notifyOnNetworkStatusChange={false}
-        >
-          {({ loading, error, data, fetchMore }) => (
-            <QueryResponse
-              {...{
-                loading,
-                error,
-                data,
-                fetchMore,
-                entity: 'merchants',
-                component: Merchant,
-              }}
-            />
-          )}
-        </Query>
-      </Page>
-    </NoSsr>
+    <Page title="Merchants">
+      <Query
+        query={merchantsQuery}
+        variables={merchantsQueryVariables}
+        fetchPolicy="cache-and-network"
+        errorPolicy="all"
+        notifyOnNetworkStatusChange={false}
+      >
+        {({ loading, error, data, fetchMore }) => (
+          <QueryResponse
+            {...{
+              loading,
+              error,
+              data,
+              fetchMore,
+              entity: 'merchants',
+              component: Merchant,
+            }}
+          />
+        )}
+      </Query>
+    </Page>
   )
 }
 
