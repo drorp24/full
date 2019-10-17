574307633f816d6bf0c4b911a690dabb543dab34
Dror <drorp24@gmail.com>
2019-10-17 18:26:48 +0300

    .

--- src/components/utility/detect.js
+++ src/components/utility/detect.js
@@ -1,8 +1,11 @@
 // Detects if device is on iOS
 export const isIos = () => {
-  const userAgent = window.navigator.userAgent.toLowerCase()
+  const userAgent =
+    typeof window === 'object' && window.navigator.userAgent.toLowerCase()
   return /iphone|ipad|ipod/.test(userAgent)
 }
 // Detects if device is in standalone mode
 export const isInStandaloneMode = () =>
-  'standalone' in window.navigator && window.navigator.standalone
+  typeof window === 'object' &&
+  'standalone' in window.navigator &&
+  window.navigator.standalone
